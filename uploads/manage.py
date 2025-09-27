1dc5d78f660d13e6124812a5d019d16982b0e54b
Uyanda <uyandasibiya@gmail.com>
2025-09-27 13:39:30 +0200

    dashboard changes

--- uploads/manage.py
+++
@@ -1,9 +0,0 @@
-from app import app, db
-from flask_migrate import Migrate
-from flask.cli import FlaskGroup
-
-migrate = Migrate(app, db)
-cli = FlaskGroup(app)
-
-if __name__ == '__main__':
-    cli()
